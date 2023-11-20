f0019c8cd296f053e94f8d82c00c05d734333a0e
komakine-anna <anna.komakine@relic.co.jp>
2023-11-20 20:51:56 +0900

    ✨ YouTubeサムネリストコンポーネントのレイアウト作成

---
+++ src/constants/mv-list-data.ts
@@ -0,0 +1,33 @@
+type MvListDataType = {
+  id: string
+  title: string
+  fullTitle: string
+}
+
+export const mvListData: MvListDataType[] = [
+  {
+    id: 'kcelgrGY1h8',
+    title: 'New Jeans',
+    fullTitle: `NewJeans (뉴진스) 'New Jeans' Official MV`
+  },
+  {
+    id: 'ArmDp-zijuc',
+    title: 'Super Shy',
+    fullTitle: `NewJeans (뉴진스) 'Super Shy' Official MV`
+  },
+  {
+    id: 's4Ow55AbdCg',
+    title: 'ETA',
+    fullTitle: `NewJeans (뉴진스) 'ETA' Official MV (Performance ver.)`
+  },
+  {
+    id: 'kKsivrgoyDw',
+    title: 'Cool With You',
+    fullTitle: `NewJeans (뉴진스) 'Cool With You' Official MV (Performance ver.)`
+  },
+  {
+    id: 'dJdqn5v4Dkw',
+    title: 'ASAP',
+    fullTitle: `NewJeans (뉴진스) 'ASAP' Official MV`
+  },
+]
